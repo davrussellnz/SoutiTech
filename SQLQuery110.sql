beb48b5280539bb6da4bc3866227a5ea88735f5f
David Russell <114596974+davrussellnz@users.noreply.github.com>
2023-10-11 14:18:38 -0400

    Update SQLQuery110.sql

--- SQLQuery110.sql
+++ SQLQuery110.sql
@@ -1,17 +1,1 @@
-﻿INSERT INTO Priorite (Nom, Niveau)
-VALUES ('High', 3), 
-       ('Medium', 2), 
-       ('Low', 1);
-INSERT INTO Categorie (Nom)
-VALUES 
-('Technical Issue'),
-( 'Billing Inquiry'),
-( 'Sales Question'),
-( 'Other'),
-('Feature Request'),
-( 'Bug Report'),
-( 'Account Change'),
-('Account Termination'),
-( 'Account Creation'),
-( 'Account Billing'),
-( 'Account Login');
+
