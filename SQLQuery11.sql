5363b2c143d4ff735157ec7479836f1da9721f30
David Russell <114596974+davrussellnz@users.noreply.github.com>
2023-10-11 14:18:01 -0400

    Update SQLQuery11.sql

--- SQLQuery11.sql
+++ SQLQuery11.sql
@@ -1,15 +1,1 @@
-﻿INSERT INTO Categorie (Id, Nom)
-VALUES 
-('Technical Issue'),
-( 'Billing Inquiry'),
-( 'Sales Question'),
-( 'Other'),
-('Feature Request'),
-( 'Bug Report'),
-( 'Account Change'),
-('Account Termination'),
-( 'Account Creation'),
-( 'Account Billing'),
-( 'Account Login');
-
 
